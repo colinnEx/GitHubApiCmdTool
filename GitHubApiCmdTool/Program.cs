95250c91ac260f5d669533b2d6dbf91ca86658a2
ColinnEx <colinn@outlook.com>
2013-12-10 21:01:30 -0800

    first working changes to get user info from GitHub api.

--- GitHubApiCmdTool/Program.cs
+++ GitHubApiCmdTool/Program.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GitHubApiCmdTool
+﻿namespace GitHubApiCmdTool
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Octokit;
+
     class Program
     {
         static void Main(string[] args)
         {
+            console_Start().Wait();
+        }
+
+        static async Task console_Start()
+        {
+            var user = await GetUser();
+
+            Console.WriteLine(user.Followers + " folks love me!");
+            Console.WriteLine("I love {0} folks!", user.Following);
+        }
+
+        static async Task<User> GetUser()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("GitHubApiCmdTool"));
+            return await github.User.Get("colinnEx");
         }
     }
 }
